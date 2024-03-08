608c40668bc9296519677b5b93b6a897e0cd917d
Pmobilee <muldamion@gmail.com>
2024-03-08 11:00:49 +0100

    sdfsdf

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -69,7 +69,7 @@ def create_argparser():
         microbatch=-1,  # -1 disables microbatches
         ema_rate="0.9999",  # comma-separated list of EMA values
         log_interval=10,
-        save_interval=500,
+        save_interval=20000,
         resume_checkpoint="",
         use_fp16=False,
         fp16_scale_growth=1e-3,
